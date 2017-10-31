228e1b7f801e1a2efe8afae9665b77d9c74ccb7e
gugu0das <gugu0das@gmail.com>
2017-10-31 21:25:53 +0900

    wcnss: Define wcnss_init_work

--- include/linux/wcnss_wlan.h
+++ include/linux/wcnss_wlan.h
@@ -116,6 +116,7 @@ u32 wcnss_get_wlan_rx_buff_count(void);
 int wcnss_wlan_iris_xo_mode(void);
 void wcnss_flush_work(struct work_struct *work);
 void wcnss_flush_delayed_work(struct delayed_work *dwork);
+void wcnss_init_work(struct work_struct *work , void *callbackptr);
 int wcnss_get_iris_name(char *iris_version);
 void wcnss_dump_stack(struct task_struct *task);
 #ifdef CONFIG_WCNSS_REGISTER_DUMP_ON_BITE
